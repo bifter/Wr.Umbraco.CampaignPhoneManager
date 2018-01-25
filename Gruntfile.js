79ff0c67477511c1dea90ac48b24a4a5dd8457bf
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-01-25 15:02:58 +0000

    Update to nuget.spec

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,12 @@
 /// <binding />
 module.exports = function (grunt) {
 
-    //require('./grunt_phonemanager.js')(grunt);
-    require('./grunt_phonemanager_personalisation.js')(grunt);
+    require('./grunt_phonemanager.js')(grunt);
+    //require('./grunt_phonemanager_personalisation.js')(grunt);
 
     grunt.loadNpmTasks('grunt-umbraco-package');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-nuget');
     grunt.loadNpmTasks('grunt-zip');
-
-    //grunt.registerTask('release_all', ['release_phonemanager', 'release_personalisation_plugin']);
-    //grunt.registerTask('default_all', ['default_phonemanager', 'default_personalisation_plugin']);
 };
