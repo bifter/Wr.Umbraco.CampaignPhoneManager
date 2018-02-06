f643bfe6b66c9d71b26075264a90d062c857f59d
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-02-06 16:45:59 +0000

    Updated UmbracoTools

--- localpackagertest.ps1
+++ localpackagertest.ps1
@@ -1,11 +1,11 @@
 
 
-(Get-Content Wr.UmbracoPhoneManager\umbraco_package.xml) -replace "<version>(.+)</version>", "<version>2.2.2</version>" | Out-File -Encoding "UTF8" Wr.UmbracoPhoneManager\umbraco_package.xml
+#(Get-Content Wr.UmbracoPhoneManager\umbraco_package.xml) -replace "<version>(.+)</version>", "<version>2.2.2</version>" | Out-File -Encoding "UTF8" Wr.UmbracoPhoneManager\umbraco_package.xml
 
-    (Get-Content Wr.UmbracoPhoneManager.Personalisation\umbraco_package.xml) -replace "<version>(.+)</version>", "<version>3.3.3</version>" | Out-File -Encoding "UTF8" Wr.UmbracoPhoneManager.Personalisation\umbraco_package.xml
+ #   (Get-Content Wr.UmbracoPhoneManager.Personalisation\umbraco_package.xml) -replace "<version>(.+)</version>", "<version>3.3.3</version>" | Out-File -Encoding "UTF8" Wr.UmbracoPhoneManager.Personalisation\umbraco_package.xml
 
-    build\tools\UmbracoTools\Wr.UmbracoTools.Packager.exe -set Wr.UmbracoPhoneManager\umbraco_package_settings.json -out "..\releases\umbraco\umbracophonemanager-{version}.zip"
+    build\tools\UmbracoTools\Wr.UmbracoTools.Packager.exe -set build\umbraco\phonemanager_settings.json -out "..\releases\umbraco\umbracophonemanager-{version}.zip"
 
-    build\tools\UmbracoTools\Wr.UmbracoTools.Packager.exe -set Wr.UmbracoPhoneManager.Personalisation\umbraco_package_settings.json -out "..\releases\umbraco\umbracophonemanager.personalisation-{version}.zip"
+    build\tools\UmbracoTools\Wr.UmbracoTools.Packager.exe -set build\umbraco\personalisation_plugin_settings.json -out "..\releases\umbraco\umbracophonemanager.personalisation-{version}.zip"
 
-	nuget pack  Wr.UmbracoPhoneManager\Wr.UmbracoPhoneManager.nuspec -OutputDirectory ..\releases\nuget\
+	nuget.exe pack  Wr.UmbracoPhoneManager\Wr.UmbracoPhoneManager.csproj -OutputDirectory ..\releases\nuget\
