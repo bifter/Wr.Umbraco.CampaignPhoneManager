5923a37353498a3aa37c449337e35e420ddfbaba
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-02-07 13:31:13 +0000

    test beta build

--- Wr.UmbracoPhoneManager/Properties/AssemblyInfo.cs
+++ Wr.UmbracoPhoneManager/Properties/AssemblyInfo.cs
@@ -33,6 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 
 // allow for unit testing of private methods
 [assembly: InternalsVisibleTo("Wr.UmbracoPhoneManager.Tests")]
